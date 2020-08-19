815b36a3189275a97eebac203d1101bfb8fa9104
Mateusz Gozdek <mgozdekof@gmail.com>
2020-08-19 08:59:17 +0200

    main.go: use local code rather than the library code
    Required after moving Terraform code into new location.
    
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 
-	"github.com/flexkube/libflexkube/cmd/terraform-provider-flexkube/flexkube"
+	"github.com/flexkube/terraform-provider-flexkube/flexkube"
 )
 
 func main() {
